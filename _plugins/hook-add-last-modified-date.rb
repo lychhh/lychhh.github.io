81fb3513e716ba68adfb563e964febf69e4f5c7c
Jevua Xu <1178280528@qq.com>
2018-05-31 16:24:39 +0800

    test last modifytime

--- _plugins/hook-add-last-modified-date.rb
+++ _plugins/hook-add-last-modified-date.rb
@@ -1,7 +1,7 @@
 Jekyll::Hooks.register :posts, :pre_render do |post|
 
   # get the current post last modified time
-  modification_time = File.mtime( post.path )
+  modification_time = File.mtime( post.category )
 
   # inject modification_time in post's datas.
   post.data['last-modified-date'] = modification_time
